2bd7392c011e5be6520d1bc93ce5ed8695a08aa7
Hojagulyyev <ovez.hojagulyyev@gmail.com>
2023-09-27 13:36:54 +0500

    fix(settings): convert to integer env DEBUG value

--- rysgally_project/settings.py
+++ rysgally_project/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = os.environ["DEBUG"]
+DEBUG = int(os.environ["DEBUG"])
 
 ALLOWED_HOSTS = ["*"]
 
