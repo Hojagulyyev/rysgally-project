ef46ee99dc769d699019b7e366955c55fce18378
Hojagulyyev <ovez.hojagulyyev@gmail.com>
2023-09-27 13:46:28 +0500

    fix: add static url to project urlpatterns

--- rysgally_project/urls.py
+++ rysgally_project/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rysgally_project.settings import NGINX_PRELOCATION
 
@@ -14,4 +16,4 @@ urlpatterns = [
     path(f"{NGINX_PRELOCATION}/", index),
     path(f"{NGINX_PRELOCATION}/commits/", include("apps.commits.urls", namespace="commits")),
     path(f"{NGINX_PRELOCATION}/users/", include("apps.users.urls", namespace="users")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
